92dc33881269b8555f999a3b75d0da68b30a3ec7
Paige Throckmorton <path4942@gmail.com>
2017-11-27 16:16:25 -0600

    Created read subcommand

--- pets.js
+++ pets.js
@@ -1,13 +1,22 @@
 let fs = require('fs');
 let path = require('path');
-
 let petsPath = path.join(__dirname, 'pets.json');
-console.log(__dirname);
 
-fs.readFile(petsPath, 'utf8', function(err, data){
-  if (err){
-    throw err;
-  }
+let node = path.basename(process.argv[0]);
+let file = path.basename(process.argv[1]);
+let cmd = process.argv[2];
+
+if (cmd === 'read'){
+  fs.readFile(petsPath, 'utf8', function(err, data){
+    if (err){
+      throw err;
+    }
+
+
   let pets = JSON.parse(data);
   console.log(pets);
 });
+} else{
+  console.error(`Usage: ${node} ${file} read`);
+  process.exit(1);
+}
