9ff72fbb730ac757fa074174f5ba2426f57591b9
Paige Throckmorton <path4942@gmail.com>
2017-11-28 14:38:29 -0600

    finished exercises moving onto bonuses

--- pets.js
+++ pets.js
@@ -1,12 +1,6 @@
 #! /usr/local/bin/node
 
-/*
- * A command line file system for a pet shop directory
- *
- * Example:
- *
- *
- */
+'use strict';
 
 let fs = require('fs');
 let http = require('http');
@@ -16,8 +10,10 @@ let petsPath = path.join(__dirname, 'pets.json');
 let node = path.basename(process.argv[0]);
 let file = path.basename(process.argv[1]);
 let cmd = process.argv[2];
-let index = process.argv[3];
 
+function numberCheck(num) {
+  return !isNaN(num)
+}
 
 if (cmd === 'read') {
   fs.readFile(petsPath, 'utf8', function(err, data) {
@@ -25,47 +21,58 @@ if (cmd === 'read') {
       throw err;
     }
     let pets = JSON.parse(data);
-    // console.log(pets);
+    let index = process.argv[3];
 
-    if (numberCheck(index)){
-      console.log(pets[index])
-    }
+    if (undefined) {
+      console.log(`Usage: ${node} ${file} read INDEX`)
+      process.exit(1);
 
-    else {
-      console.log(pets)
-    }
+    } else if
+      (numberCheck(index)) {
+          console.log(pets[index])
+        }
+          else {
+        console.log(pets)
+      }
+
   });
 
-  function numberCheck(num){
-    return !isNaN(num)
+} else if (cmd === 'create') {
+
+  let petAge = Number(process.argv[3]);
+  let petKind = process.argv[4];
+  let petName = process.argv[5];
+
+  if (petAge === undefined || petKind === undefined || petName === undefined){
+    console.error(`Usage: ${node} ${file} ${cmd} AGE KIND NAME`);
+    process.exit(1);
+  } else{
+    fs.readFile(petsPath, 'utf8', function(readErr, data) {
+      if (readErr) {
+        throw readErr;
+      }
+
+      let pets = JSON.parse(data);
+      let pet = {
+        age: petAge,
+        kind: petKind,
+        name: petName
+      };
+
+      pets.push(pet);
+      let petsJSON = JSON.stringify(pets);
+      fs.writeFile(petsPath, petsJSON, function(writeErr) {
+        if (writeErr) {
+          throw writeErr;
+        }
+        console.log(pet);
+      });
+    });
   }
 
 
-  
-
-} else if (cmd === 'create') {
-  fs.readFile(petsPath, 'utf8', function(readErr, data) {
-    if (readErr) {
-      throw readErr;
-    }
-
-    let pets = JSON.parse(data);
-    let pet = process.argv[3];
-    if (!pets) {
-      console.error(`Usage: ${node} ${file} ${cmd} PET`);
-      process.exit(1);
-    }
-    pets.push(pet);
-
-    let petsJSON = JSON.stringify(pets);
-    fs.writeFile(petsPATH, petsJSON, function(writeERR) {
-      if (writeErr) {
-        throw writeErr;
-      }
-      console.log(pet);
-    });
-  });
 } else {
   console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
   process.exit(1);
+
 }
