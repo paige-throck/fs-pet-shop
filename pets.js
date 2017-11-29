229026749f41348df2b5e766067a418715320057
Paige Throckmorton <path4942@gmail.com>
2017-11-29 09:10:38 -0600

    added update command

--- pets.js
+++ pets.js
@@ -1,4 +1,4 @@
-#! /usr/local/bin/node
+#!/usr/bin/env node
 
 'use strict';
 
@@ -23,7 +23,7 @@ if (cmd === 'read') {
     let pets = JSON.parse(data);
     let index = process.argv[3];
 
-    if (index > pets.length || index < pets.length - pets.length + 1) {
+    if (index > pets.length || index < pets.length - pets.length) {
       console.log(`Usage: ${node} ${file} ${cmd} INDEX`)
       process.exit(1);
 
@@ -71,36 +71,45 @@ if (cmd === 'read') {
   }
 } else if (cmd === 'update') {
   fs.readFile(petsPath, 'utf8', function(err, data) {
+      if (err) {
+        throw err;
+      }
+
+      if (!process.argv[3] || !process.argv[4] || !process.argv[5] || !process.argv[6]) {
+        console.error(`Usage: ${node} ${file} ${cmd} INDEX AGE KIND NAME`);
+        process.exit(1);
+
+      }
+      let newPets = JSON.parse(data)
+      let newIndex = process.argv[3]
+      let newAge = Number(process.argv[4])
+      let newKind = process.argv[5]
+      let newName = process.argv[6]
+
+      newPets[newIndex] = {
+        age: newAge,
+        kind: newKind,
+        name: newName
+      }
+
+      console.log(newPets[newIndex]);
+
+      let petsJSON = JSON.stringify(newPets);
+      fs.writeFile(petsPath, petsJSON, function(writeErr) {
+        if (writeErr) {
+          throw writeErr;
+        }
+
+      });
+  });
+
+} else if (cmd === 'destroy') {
+  fs.readFile(petsPath, 'utf8', function(err, data) {
     if (err) {
       throw err;
     }
-    let pets = JSON.parse(data);
-    let newIndex = process.argv[3];
-    let newAge = Number(process.argv[4]);
-    let newKind = process.argv[5];
-    let newName = process.argv[6];
-    let newPet = {
-      age: newAge,
-      kind: newKind,
-      name: newName
-    };
-
-    if (newIndex > pets.length || newIndex < pets.length - pets.length + 1 || newAge === undefined || newKind === undefined || newName === undefined) {
-      console.error(`Usage: ${node} ${file} ${cmd} INDEX AGE KIND NAME`);
-
-    } else if (numberCheck(newIndex)) {
-        let newPet = newIndex
-      pets.push(newPet);
-    }
-    let petsJSON = JSON.stringify(pets);
-    fs.writeFile(petsPath, petsJSON, function(writeErr) {
-      if (writeErr) {
-        throw writeErr;
-      }
-      console.log(newPet);
-    });
-
   });
+
 } else {
   console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
   process.exit(1);
