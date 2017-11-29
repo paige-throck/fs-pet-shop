df0ccb030654bdf8d232310c5989ccd199310428
Paige Throckmorton <path4942@gmail.com>
2017-11-29 12:44:25 -0600

    Passed first three tests

---
+++ httpServer.js
@@ -0,0 +1,72 @@
+'use strict';
+
+let fs = require('fs');
+let path = require('path');
+let petsPath = path.join(__dirname, 'pets.json');
+
+let http = require('http');
+let port = process.env.PORT || 8000;
+
+
+let server = http.createServer(function(req, res) {
+  if (req.method === 'GET' && req.url === '/pets') {
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(petsJSON);
+    });
+  }
+
+  else if (req.method === 'GET' && req.url === '/pets/0') {
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      let pets = JSON.parse(petsJSON);
+      let petJSON = JSON.stringify(pets[0]);
+
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(petJSON);
+
+    });
+  }
+  else if (req.method === 'GET' && req.url === '/pets/1') {
+    fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
+
+      let pets = JSON.parse(petsJSON);
+      let petJSON = JSON.stringify(pets[1]);
+
+      res.setHeader('Content-Type', 'application/json');
+      res.end(petJSON);
+    });
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Not found');
+  }
+});
+
+server.listen(port, function() {
+  console.log('Listening on port', port);
+});
+
+
+module.exports = server;
